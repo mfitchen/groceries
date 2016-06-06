c0becc5a4c02419e53471f80e9ea89aa56416c98
Marty Fitchen and Matt Wilkin <marty@>
2016-06-06 16:17:20 -0700

    refix and refinalize jQuery to display an aplhabetical and UPPERCASE list

--- js/scripts.js
+++ js/scripts.js
@@ -3,30 +3,18 @@ $(document).ready(function() {
     event.preventDefault();
 
     var blanks = ["item1", "item2", "item3", "item4", "item5"];
-
     var newArray = [];
 
     blanks.forEach(function(blank) {
       var userInput = $("input#" + blank).val();
        newArray.push(userInput.toUpperCase());
-      //$("." + blank).text(userInput);
     });
 
-    newArray = newArray.sort();
+    var newArray = newArray.sort();
+
     newArray.forEach(function(item) {
-      //build a list here
       $(".items").append("<li>" + item + "</li>");
-      //concat item with <li></li>
-
-
     });
-    // var arrays = blanks.map(function(blank) {
-    //   return blank.toUpperCase();
-    // });
-
-
-
-    // var newArray = newArray.sort();
 
     $("#grcery-list").show();
     $("#blanks").hide();
